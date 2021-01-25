14d049124dbd4c149ce565e8f02f25c478608c8b
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-01-25 03:55:40 -0500

    Exersice 1) added comments

--- Exercise1/js/script.js
+++ Exercise1/js/script.js
@@ -28,13 +28,14 @@ let bg={
 
 
 function preload(){
+  //loading bacteria image
   for(let i = 0; i < NUM_BACTERIA_IMAGES; i++){
     let bacteriaImage =  loadImage(`assets/images/bacteria${i}.png`);
     bacteriaImages.push(bacteriaImage);
   }
-
+  //loading the virus image
   virusImage = loadImage(`assets/images/virus.png`);
-
+  //loading disinfection spray sound
   disinfection= loadSound(`assets/sounds/spray.mp3`);
 }
 
@@ -45,20 +46,30 @@ function setup() {
   textAlign(CENTER,CENTER);
 
   for(let i=0; i<numBacteria; i++){
+    //random loaction for bacterias
     let x= random(0,width);
     let y= random(0,height);
+    //random rotation for bacterias
     let rotation=random(0.1,1);
+    //reloacting the bacterias everytime they disapear from the screen
     let relocate= random(0,50);
     let bacteriaImage=random(bacteriaImages);
+    //constructing bacterias
     let bacteria = new Bacteria(x,y,bacteriaImage,relocate,rotation);
     bacterias.push(bacteria);
   }
 
-  let relocate= random(0,50);
+  //random loaction for virus
   let x= random(0,width);
   let y= random(0,height);
+  //random rotation for bacterias
+  let rotation=random(0.1,1);
+  //reloacting the virus everytime it disapears from the screen
+  let relocate= random(0,50);
+  //constructing the virus
   virus= new Virus(x,y,virusImage)
 
+  //constructiong the timer
   timer= new Timer();
 }
 
@@ -68,22 +79,26 @@ function draw() {
     push();
     background(bg.r,bg.g,bg.b);
     fill(255);
-    text('click right.',width/2,height/2);
+    //instruction
+    text('There is a stubborn virus among the bacterias. It has to be cuaght before it becomes toxic. The time considered to complete this mission is on the left. ',width/2,height/2);
+    text('Everytime you click on the wrong one you lose a certain amout of time. Try to find it before they fade cause each time their location changes.',width/2,height/2+100);
+    //picture of what the virus looks like
     virus.demo();
     pop();
 
     fill(100,50,67);
     rectMode(CENTER);
-    text('choose',width/2,height/2+300)
+    text('Choose the level of difficulty. On keyboard 1-2-3.',width/2,height/2+300)
+    //level of difficulty
     ellipse(width/2-200,height/2+400,130)
     ellipse(width/2+200,height/2+400,130)
     ellipse(width/2,height/2+400,130)
 
     push();
     fill(255)
-    text('easy',width/2-200,height/2+400)
-    text('medium',width/2,height/2+400)
-    text('hard',width/2+200,height/2+400)
+    text('Easy',width/2-200,height/2+400)
+    text('Medium',width/2,height/2+400)
+    text('Hard',width/2+200,height/2+400)
     pop();
 
     }
@@ -91,24 +106,28 @@ function draw() {
     else if(state==='start'){
       background(bg.r,bg.g,bg.b);
 
+      //displaying bacterias
       for(let i = 0; i<bacterias.length; i++){
         bacterias[i].update();
       }
 
+      //displaying th virus
       virus.update();
 
       //displaying timer and showing the time that is left
       timer.display();
       timer.shrink();
-      //when time is overm shoe the ending titration(win/lose)
+      //when time is over they lose
       timer.timeOver();
-      //ending titration based on if you have whon or lose
+
+      //show the losing titration
       lost();
       won();
     }
   }
 
   function mousePressed(){
+    //check if the virus is found
     virus.mousePressed();
   }
 
@@ -116,6 +135,7 @@ function draw() {
     if(keyCode===49 &&
       state==='title'){
         state='start';
+        //if chosen the easy level the timer runs slower
         timer.diminish=-0.1
       }
 
@@ -128,6 +148,7 @@ function draw() {
       else if(keyCode===51 &&
         state==='title'){
           state='start';
+          //if chosen the easy level the timer runs fatser
           timer.diminish=-7
         }
       }
@@ -136,6 +157,7 @@ function draw() {
         if (state === 'lose'){
           background(150,0,0)
           fill(255);
+          //show the losing titration
           text('fuck' ,width/2,height/2)
         };
       };
@@ -145,6 +167,7 @@ function draw() {
         if (state === 'win'){
           background(0,150,0)
           fill(255);
+          //show the winning titration
           text('yay' ,width/2,height/2)
         };
       }
