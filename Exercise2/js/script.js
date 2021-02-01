796fbba56d9c87456569f6bd78142a8687875882
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-01-31 21:47:02 -0500

    Exercise2) working on responsive voice

--- Exercise2/js/script.js
+++ Exercise2/js/script.js
@@ -8,25 +8,169 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-/**
-Description of preload
-*/
-function preload() {
+const animals=[
+     "aardvark",
+     "alligator",
+     "alpaca",
+     "antelope",
+     "ape",
+     "armadillo",
+     "baboon",
+     "badger",
+     "bat",
+     "bear",
+     "beaver",
+     "bison",
+     "boar",
+     "buffalo",
+     "bull",
+     "camel",
+     "canary",
+     "capybara",
+     "cat",
+     "chameleon",
+     "cheetah",
+     "chimpanzee",
+     "chinchilla",
+     "chipmunk",
+     "cougar",
+     "cow",
+     "coyote",
+     "crocodile",
+     "crow",
+     "deer",
+     "dingo",
+     "dog",
+     "donkey",
+     "dromedary",
+     "elephant",
+     "elk",
+     "ewe",
+     "ferret",
+     "finch",
+     "fish",
+     "fox",
+     "frog",
+     "gazelle",
+     "gila monster",
+     "giraffe",
+     "gnu",
+     "goat",
+     "gopher",
+     "gorilla",
+     "grizzly bear",
+     "ground hog",
+     "guinea pig",
+     "hamster",
+     "hedgehog",
+     "hippopotamus",
+     "hog",
+     "horse",
+     "hyena",
+     "ibex",
+     "iguana",
+     "impala",
+     "jackal",
+     "jaguar",
+     "kangaroo",
+     "koala",
+     "lamb",
+     "lemur",
+     "leopard",
+     "lion",
+     "lizard",
+     "llama",
+     "lynx",
+     "mandrill",
+     "marmoset",
+     "mink",
+     "mole",
+     "mongoose",
+     "monkey",
+     "moose",
+     "mountain goat",
+     "mouse",
+     "mule",
+     "muskrat",
+     "mustang",
+     "mynah bird",
+     "newt",
+     "ocelot",
+     "opossum",
+     "orangutan",
+     "oryx",
+     "otter",
+     "ox",
+     "panda",
+     "panther",
+     "parakeet",
+     "parrot",
+     "pig",
+     "platypus",
+     "polar bear",
+     "porcupine",
+     "porpoise",
+     "prairie dog",
+     "puma",
+     "rabbit",
+     "raccoon",
+     "ram",
+     "rat",
+     "reindeer",
+     "reptile",
+     "rhinoceros",
+     "salamander",
+     "seal",
+     "sheep",
+     "shrew",
+     "silver fox",
+     "skunk",
+     "sloth",
+     "snake",
+     "squirrel",
+     "tapir",
+     "tiger",
+     "toad",
+     "turtle",
+     "walrus",
+     "warthog",
+     "weasel",
+     "whale",
+     "wildcat",
+     "wolf",
+     "wolverine",
+     "wombat",
+     "woodchuck",
+     "yak",
+     "zebra"
+   ];
 
-}
+let currentAnimal =``;
 
 
-/**
-Description of setup
-*/
 function setup() {
 
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
 
+}
+
+
+function mousePressed(){
+  currentAnimal= random(animals);
+  let brokenString= breakString(currentAnimal);
+  responsiveVoice.speak(brokenString);
+}
+
+
+/**
+Reverses the provided string
+*/
+function breakString(string) {
+  // Split the string into an array of characters
+  let characters = string.split('');
+  // Return the result
+  return characters;
 }
