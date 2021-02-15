b715df12716d60cc6bac06dbb470e45d74182aa6
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-02-15 17:52:13 -0500

    Exercise4) added circles for the submarine to catch

--- Exercise4/js/script.js
+++ Exercise4/js/script.js
@@ -20,6 +20,8 @@ let score=0;
 let jellyFishSquad=[];
 let squadSize=280;
 
+let bubble=undefined;
+
 
 let bg={
   r:0,
@@ -64,6 +66,16 @@ function setup() {
   for (let i = 0; i < squadSize; i++){
     jellyFishSquad[i]= createjellyFish(random(0,width),random(-5000,0));
   };
+
+  //bubbles
+  bubble={
+    x:random(width),
+    y:0,
+    size:50,
+    vx:0,
+    vy:4
+  }
+
 }
 
 
@@ -105,22 +117,46 @@ function draw() {
     image(submarine.image,submarine.x,submarine.y,d2,d2)
 
 
-      for (let i = 0; i<jellyFishSquad.length; i++){
-        let jellyFish=jellyFishSquad[i];
-        push();
-        fill(235, 180, 52);
-        ellipse(jellyFish.x,jellyFish.y,jellyFish.size);
-        pop();
+    for (let i = 0; i<jellyFishSquad.length; i++){
+      let jellyFish=jellyFishSquad[i];
+      push();
+      fill(235, 180, 52);
+      ellipse(jellyFish.x,jellyFish.y,jellyFish.size);
+      pop();
 
-        jellyFish.y=jellyFish.y+jellyFish.speed
+      jellyFish.y=jellyFish.y+jellyFish.speed
 
 
 
-        //check
-        let d=dist(submarine.x,submarine.y,jellyFish.x,jellyFish.y);
-        if(d<jellyFish.size/2+d2/3){
-          bg.r=100
-        }
+      //check
+      let d=dist(submarine.x,submarine.y,jellyFish.x,jellyFish.y);
+      if(d<jellyFish.size/2+d2/3){
+        bg.r=100
+        score=score-1
       }
+    }
+
+    //move the bubbles
+    bubble.x+=bubble.vx;
+    bubble.y+=bubble.vy;
+
+    if(bubble.y>windowHeight){
+      bubble.x=random(0,width);
+      bubble.y=0;
+    }
+
+    push();
+    fill(0,100,200);
+    noStroke();
+    ellipse(bubble.x,bubble.y,bubble.size);
+    pop();
+
+    let d3=dist(bubble.x,bubble.y,submarine.x,submarine.y)
+    if(d3<bubble.size/2+d2/2){
+      bg.g=200
+      bubble.x=random(0,width);
+      bubble.y=0;
+      score++
+    }
   }
 }
