ba596cb018e133480209736ace0e0e3a3791199f
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-02-15 18:44:01 -0500

    Exercise4) added comments

--- Exercise4/js/script.js
+++ Exercise4/js/script.js
@@ -23,6 +23,7 @@ let jellyFishSquad=[];
 let squadSize=280;
 
 let fish=undefined;
+
 let state ='title';
 
 let bg={
@@ -36,10 +37,10 @@ let submarine={
   y:0,
   size:200,
   image:undefined
-}
+};
 
 function preload() {
-  submarine.image=loadImage(`assets/images/submarine.png`)
+  submarine.image=loadImage(`assets/images/submarine.png`);
 }
 
 
@@ -67,12 +68,12 @@ function setup() {
     predictions= results;
   });
 
-
+  //creating jellyfish at random positions
   for (let i = 0; i < squadSize; i++){
     jellyFishSquad[i]= createjellyFish(random(0,width),random(-5000,0));
   };
 
-  //bubbles
+  //the fishe the submarine has to collect
   fish={
     x:random(width),
     y:0,
@@ -112,7 +113,7 @@ function draw() {
     //displaying timer and showing the time that is left
     timer.display();
     timer.shrink();
-    //when time is over they lose
+    //when time is over they lose or win according to the score they have
     timer.timeOver();
 
 
@@ -120,10 +121,9 @@ function draw() {
     push()
     fill(255,0,0)
     text('Your Score: ' + score,100,40);
+    pop();
 
-    lost();
-    won();
-
+    //if there are predictions to show objects of the game
     if(predictions.length>0){
       let hand= predictions[0];
       let index= hand.annotations.indexFinger;
@@ -134,17 +134,20 @@ function draw() {
       let baseX= base[0];
       let baseY= base[1];
 
-
+      //the position of the submarine machine is based on the position of the palm
       submarine.x= baseX
       submarine.y=baseY
+
+      //measure the distance between the tip and the base
       let d2= dist(baseX,baseY,tipX,tipY)
 
       //submarine machine
       imageMode(CENTER);
+      //the distance of the base and index's tip as the image size
       submarine.size=d2
       image(submarine.image,submarine.x,submarine.y,submarine.size,submarine.size)
 
-
+      //dipsplaying the jellyfish
       for (let i = 0; i<jellyFishSquad.length; i++){
         let jellyFish=jellyFishSquad[i];
         push();
@@ -153,32 +156,35 @@ function draw() {
         ellipse(jellyFish.x,jellyFish.y,jellyFish.size);
         pop();
 
+        // moving the jellyfish
         jellyFish.y=jellyFish.y+jellyFish.speed
+        //increasing the jelyfish's speed by time
         jellyFish.speed=jellyFish.speed+jellyFish.speedIncrease;
 
-        //check
+        //check if the jelyfish has touched the submarine machine
         let d=dist(submarine.x,submarine.y,jellyFish.x,jellyFish.y);
         if(d<jellyFish.size/2+d2/3){
           score=score-1
         }
       }
 
-
-      //move the bubbles
+      //move the fish
       fish.x+=fish.vx;
       fish.y+=fish.vy;
-
+      //if the fish is out of screen create a new one at random position
       if(fish.y>windowHeight){
         fish.x=random(0,width);
         fish.y=0;
       }
 
+      //displaying the fish
       push();
       fill(0,100,200);
       noStroke();
       ellipse(fish.x,fish.y,fish.size);
       pop();
 
+      //check if the fish has been caught
       let d3=dist(fish.x,fish.y,submarine.x,submarine.y)
       if(d3<fish.size/2+d2/2){
         fish.x=random(0,width);
@@ -187,6 +193,8 @@ function draw() {
       }
     }
   }
+
+  //check if their score is at the determined rate to check if they have won or lost
   lost();
   won();
 
