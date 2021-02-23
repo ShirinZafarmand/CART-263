6ea56f06b6fdf725fdb183cfa7b83c76cbfd593f
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-02-23 01:49:28 -0500

    Project1) the visiblity triangle stone

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -7,6 +7,13 @@ Shirin Zafarmand
 
 let state ='title';
 
+//user's webcam
+let video= undefined;
+//handpose object
+let handpose= undefined;
+// the  predictions made once it's running
+let predictions =[];
+
 let bg={
   r:0,
   g:0,
@@ -22,26 +29,49 @@ let circus={
   image:undefined
 }
 
+let buttonShadow={
+  x:0,
+  y:0,
+  width:600,
+  height:600,
+  image:undefined
+}
+
+
 function preload() {
 circus.image=loadImage(`assets/images/background.jpg`);
+buttonShadow.image=loadImage(`assets/images/triangleStone2.png`);
 }
 
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  //start the webcam and hide the html element
+  video= createCapture(VIDEO);
+  video.hide();
+
+  //loading state
+  handpose = ml5.handpose(video, {
+    flipHorizontal:true
+  }, function(){
+    console.log(`model loaded.`);
+  });
+
+  //predict event
+  handpose.on(`predict`, function(results) {
+    console.log(results);
+    predictions= results;
+  });
 }
 
 
 function draw() {
   background(bg.r,bg.g,bg.b);
+
   imageMode(CORNER);
-  circus.width=windowWidth+3000
-  circus.height=windowHeight+2000
+  circus.width=windowWidth+2000
+  circus.height=windowHeight+1000
   image(circus.image,circus.x,circus.y,circus.width,circus.height);
-
-
-  circus.x=constrain(circus.x,-circus.width+windowWidth,0);
-  circus.y=constrain(circus.y,-circus.height+windowHeight,0);
 
   if (keyIsDown(39)){
     circus.x=circus.x-5;
@@ -55,4 +85,26 @@ function draw() {
   if (keyIsDown(38)){
     circus.y=circus.y+4
   };
+
+  //if there are predictions to show objects of the game
+  if(predictions.length>0){
+    let hand= predictions[0];
+    let index= hand.annotations.indexFinger;
+    let tip= index[3];
+    let base= index[0];
+    let tipX= tip[0];
+    let tipY= tip[1];
+    let baseX= base[0];
+    let baseY= base[1];
+
+
+
+    buttonShadow.x= baseX;
+    buttonShadow.y=baseY;
+
+    imageMode(CORNER);
+    image(buttonShadow.image,buttonShadow.x,buttonShadow.y,buttonShadow.width,buttonShadow.height);
+    circus.x=constrain(circus.x,-circus.width+windowWidth,0);
+    circus.y=constrain(circus.y,-circus.height+windowHeight,0);
+  }
 }
