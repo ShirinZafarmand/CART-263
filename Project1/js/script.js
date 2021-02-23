ea9f5b5b81967841082120a259f8acddc2cfb0ba
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-02-22 22:35:38 -0500

    Project1)  one of the animation scenes as the background

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -5,17 +5,41 @@ Shirin Zafarmand
 
 "use strict";
 
+let state ='title';
+
+let bg={
+  r:0,
+  g:0,
+  b:0,
+};
+
+
+let circus={
+  x:0,
+  y:0,
+  width:4800,
+  height:2300,
+  image:undefined
+}
 
 function preload() {
-
+circus.image=loadImage(`assets/images/background.jpg`);
 }
 
 
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
 
 function draw() {
+  background(bg.r,bg.g,bg.b);
+  imageMode(CORNER);
+  circus.width=windowWidth+3000
+  circus.height=windowHeight+2000
+  image(circus.image,circus.x,circus.y,circus.width,circus.height);
 
+
+  circus.x=constrain(circus.x,-circus.width+windowWidth,0);
+  circus.y=constrain(circus.y,-circus.height+windowHeight,0);
 }
