460079ee9b2a26809a21f147e780b708715946dd
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-02-25 16:37:27 -0500

    Project1) debugging the score

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -64,21 +64,24 @@ let ghostEye1={
   x:310,
   y:750,
   size:50,
-  display:false
+  display:false,
+  found:false
 }
 
 let ghostEye2={
   x:1270,
   y:950,
   size:30,
-  display:false
+  display:false,
+  found:false
 }
 
 let ghostEye3={
   x:2400,
   y:940,
   size:80,
-  display:false
+  display:false,
+  found:false
 }
 
 function preload() {
@@ -119,7 +122,10 @@ function setup() {
 function draw() {
   if(state==='title'){
     //playing introduction audio(an audio straight out of the animation)
-    introductionAudio.play();
+    if(introductionAudio.isPlaying()==false){
+      introductionAudio.play();
+    }
+
   }
   else if(state==='start'){
     //stopping the audio when the game starts
@@ -130,12 +136,12 @@ function draw() {
     background(7,7,7);
 
     //3 ghost children image
-    imageMode(CENTER)
-    childrenGhosts.x=windowWidth/2
-    childrenGhosts.y=windowHeight/2
+    imageMode(CENTER);
+    childrenGhosts.x=windowWidth/2;
+    childrenGhosts.y=windowHeight/2;
 
-    childrenGhosts.width=0.65*windowWidth
-    childrenGhosts.height=windowHeight
+    childrenGhosts.width=0.65*windowWidth;
+    childrenGhosts.height=windowHeight;
     image(childrenGhosts.image,childrenGhosts.x,childrenGhosts.y,childrenGhosts.width,childrenGhosts.height);
 
     // the instruction of the game
@@ -144,9 +150,9 @@ function draw() {
     textSize(26);
     text('Find our eyes. use the triangle stone to find them,',5.5*width/10,5*height/6);
     text('they are only visible through th circle of the stone.',5.5*width/10,5*height/6+30);
-    text('Move your hand an press the arrow keys to vhange the view.',5.5*width/10,5*height/6+60)
-    text('find them before the button takes up the screen.',5.5*width/10,5*height/6+90)
-    text('Hurry! press the space key to start!',5.5*width/10,5*height/6+120)
+    text('Move your hand an press the arrow keys to vhange the view.',5.5*width/10,5*height/6+60);
+    text('find them before the button takes up the screen.',5.5*width/10,5*height/6+90);
+    text('Hurry! press the space key to start!',5.5*width/10,5*height/6+120);
     pop();
 
   }
@@ -157,43 +163,20 @@ function draw() {
 
     //displaying the background image of the circus
     imageMode(CORNER);
-    circus.width=6/5*windowWidth
-    circus.height=windowHeight
+    circus.width=6/5*windowWidth;
+    circus.height=windowHeight;
     image(circus.image,circus.x,circus.y,circus.width,circus.height);
 
     //pressing the arrow keys to help exploring the room
-    if (keyIsDown(39)){
-      circus.x=circus.x-5;
-      ghostEye1.x=ghostEye1.x-5
-      ghostEye2.x=ghostEye2.x-5
-      ghostEye3.x=ghostEye3.x-5
-    };
-    if (keyIsDown(37)){
-      circus.x=circus.x+5;
-      ghostEye1.x=ghostEye1.x+5
-      ghostEye2.x=ghostEye2.x+5
-      ghostEye3.x=ghostEye3.x+5
-    };
-    if (keyIsDown(38)){
-      circus.y=circus.y+5;
-      ghostEye1.y=ghostEye1.y+5
-      ghostEye2.y=ghostEye2.y+5
-      ghostEye3.y=ghostEye3.y+5
-    };
-    if (keyIsDown(40)){
-      circus.y=circus.y-5;
-      ghostEye1.y=ghostEye1.y-5
-      ghostEye2.y=ghostEye2.y-5
-      ghostEye3.y=ghostEye3.y-5
-    };
+    screenMovements();
 
     //displaying the button shadow image
-    push()
-    imageMode(CENTER)
-    buttonShadow.x=windowWidth/2
-    buttonShadow.y=windowHeight/2
-    image(buttonShadow.image,buttonShadow.x,buttonShadow.y,buttonShadow.width,buttonShadow.height)
-    pop()
+    push();
+    imageMode(CENTER);
+    buttonShadow.x=windowWidth/2;
+    buttonShadow.y=windowHeight/2;
+    image(buttonShadow.image,buttonShadow.x,buttonShadow.y,buttonShadow.width,buttonShadow.height);
+    pop();
 
     //if there are predictions, the triangle stone and eye balls start to apear and function
     if(predictions.length>0){
@@ -207,52 +190,15 @@ function draw() {
       let baseY= base[1];
 
       //if the triangle stone is over eye balls, they appear on the screen
-      let d1= dist(triangleStone.x,triangleStone.y,ghostEye1.x,ghostEye1.y)
-      if(d1<=70){
-        ghostEye1.display=true;
-        //once the eye number 1 is found, the score goes higher
-        score++
-      }
-      else{
-        ghostEye1.display=false;
-      }
-      if(ghostEye1.display===true){
-        fill(0, 255, 238)
-        //diplaying the eye balls number 1
-        ellipse(ghostEye1.x,ghostEye1.y,ghostEye1.size)
-      }
+         //once the eye number 1 is found, the score goes higher
+         stone1Discovery();
 
+         //once the eye number 2 is found, the score goes higher
+         stone2Discovery();
 
-      let d2= dist(triangleStone.x,triangleStone.y,ghostEye2.x,ghostEye2.y)
-      if(d2<=70){
-        ghostEye2.display=true;
-        //once the eye number 2 is found, the score goes higher
-        score++
-      }
-      else{
-        ghostEye2.display=false;
-      }
-      if(ghostEye2.display===true){
-        fill(187, 0, 255)
-        //diplaying the eye balls number 2
-        ellipse(ghostEye2.x,ghostEye2.y,ghostEye2.size)
-      }
+         //once the eye number 3 is found, the score goes higher
+         stone3Discovery();
 
-
-      let d3= dist(triangleStone.x,triangleStone.y,ghostEye3.x,ghostEye3.y)
-      if(d3<=70){
-        ghostEye3.display=true;
-        //once the eye number 3 is found, the score goes higher
-        score++
-      }
-      else{
-        ghostEye3.display=false;
-      }
-      if(ghostEye3.display===true){
-        fill(255)
-        //diplaying the eye balls number 3
-        ellipse(ghostEye3.x,ghostEye3.y,ghostEye3.size)
-      }
 
       //the location of the triangle stone is where the hand is
       triangleStone.x= baseX;
@@ -273,25 +219,8 @@ function draw() {
     text(score + ' out of 3 eyes collected' ,180,40);
     pop();
 
-    //if the button shadow takes up almost the whole screen check the score
-    buttonShadow.height=buttonShadow.height-buttonShadow.shrink
-    buttonShadow.width=buttonShadow.width-1.05*buttonShadow.shrink
-
-    //if the have collected all three eye balls, they win
-    if(buttonShadow.height<windowHeight || buttonShadow.width<windowWidth && score>=3){
-      background(66, 74, 73)
-      fill(255);
-      //show the winning titration
-      text('Thank You for saving us!' ,width/2,height/2);
-    }
-
-    //if the haven't collected all three eye balls, they lsoe
-    if(buttonShadow.height<windowHeight || buttonShadow.width<windowWidth && score<3){
-      background(66, 74, 73)
-      fill(255);
-      //show the losing titration
-      text('Oh no it is too late...' ,width/2,height/2);
-    }
+    // button shadow shrinks and functions as a timer
+    buttonShadowShrinking();
   }
 }
 
@@ -302,3 +231,116 @@ function keyPressed(){
       state='start';
     };
   }
+
+//pressing the arrow keys to help exploring the room
+function screenMovements(){
+  if (keyIsDown(39)){
+    circus.x=circus.x-5;
+    ghostEye1.x=ghostEye1.x-5;
+    ghostEye2.x=ghostEye2.x-5;
+    ghostEye3.x=ghostEye3.x-5;
+  };
+  if (keyIsDown(37)){
+    circus.x=circus.x+5;
+    ghostEye1.x=ghostEye1.x+5;
+    ghostEye2.x=ghostEye2.x+5;
+    ghostEye3.x=ghostEye3.x+5;
+  };
+  if (keyIsDown(38)){
+    circus.y=circus.y+5;
+    ghostEye1.y=ghostEye1.y+5;
+    ghostEye2.y=ghostEye2.y+5;
+    ghostEye3.y=ghostEye3.y+5;
+  };
+  if (keyIsDown(40)){
+    circus.y=circus.y-5;
+    ghostEye1.y=ghostEye1.y-5;
+    ghostEye2.y=ghostEye2.y-5;
+    ghostEye3.y=ghostEye3.y-5;
+  };
+}
+
+function stone1Discovery(){
+  let d1= dist(triangleStone.x,triangleStone.y,ghostEye1.x,ghostEye1.y);
+  if(d1<=70){
+    ghostEye1.display=true;
+  }
+  else{
+    ghostEye1.display=false;
+  }
+  if(ghostEye1.found==false){
+    //once the eye number 2 is found, the score goes higher
+    score++;
+    ghostEye1.found=true;
+  }
+  else{
+    ghostEye1.found=false;
+  }
+  if(ghostEye1.display===true){
+    fill(0, 255, 238);
+    //diplaying the eye balls number 1
+    ellipse(ghostEye1.x,ghostEye1.y,ghostEye1.size);
+  }
+}
+
+function stone2Discovery(){
+  let d2= dist(triangleStone.x,triangleStone.y,ghostEye2.x,ghostEye2.y);
+  if(d2<=70){
+    ghostEye2.display=true;
+  }
+  else{
+    ghostEye2.display=false;
+  }
+  if(ghostEye2.found==false){
+    //once the eye number 2 is found, the score goes higher
+    score++;
+    ghostEye2.found=true;
+  }
+  if(ghostEye2.display===true){
+    fill(100,0,0);
+    //diplaying the eye balls number 2
+    ellipse(ghostEye2.x,ghostEye2.y,ghostEye2.size);
+  }
+}
+
+function stone3Discovery(){
+  let d3= dist(triangleStone.x,triangleStone.y,ghostEye3.x,ghostEye3.y)
+  if(d3<=70){
+    ghostEye3.display=true;
+  }
+  else{
+    ghostEye3.display=false;
+  }
+  if(ghostEye3.found==false){
+    //once the eye number 2 is found, the score goes higher
+    score++;
+    ghostEye3.found=true;
+  }
+  if(ghostEye3.display===true){
+    fill(255)
+    //diplaying the eye balls number 3
+    ellipse(ghostEye3.x,ghostEye3.y,ghostEye3.size);
+  }
+}
+
+function buttonShadowShrinking(){
+  //if the button shadow takes up almost the whole screen check the score
+  buttonShadow.height=buttonShadow.height-buttonShadow.shrink;
+  buttonShadow.width=buttonShadow.width-1.05*buttonShadow.shrink;
+
+  //if the have collected all three eye balls, they win
+  if(buttonShadow.height<windowHeight || buttonShadow.width<windowWidth && score>=3){
+    background(66, 74, 73);
+    fill(255);
+    //show the winning titration
+    text('Thank You for saving us!' ,width/2,height/2);
+  }
+
+  //if the haven't collected all three eye balls, they lsoe
+  if(buttonShadow.height<windowHeight || buttonShadow.width<windowWidth && score<3){
+    background(66, 74, 73);
+    fill(255);
+    //show the losing titration
+    text('Oh no it is too late...' ,width/2,height/2);
+  }
+}
