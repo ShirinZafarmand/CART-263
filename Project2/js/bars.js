b010475d1544f1c8c5b9a2da91c7a6663f7e0820
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-03-22 21:21:52 -0400

    Project2) drawing the bars

--- Project2/js/bars.js
+++ Project2/js/bars.js
@@ -11,4 +11,26 @@ class Module{
     this.speed = 1;
     this.forward = true;
   }
+
+  draw(){
+    push();
+    let widthExtra;
+    translate(this.x, this.y);
+    noStroke();
+    fill(12);
+    //using quad instead of rect to set slope for the bars
+    quad(this.j,0,this.j+this.tan,0,this.j+this.tan/2,height,this.j,height);
+    //slowly increasing the slope
+    this.tan=this.tan+0.01;
+    //the movement speed for the bars
+    this.x = this.x + this.speed;
+    if(this.x > widthExtra){
+      this.x = -u;
+    }
+    if(this.x < -u){
+      this.x = widthExtra;
+    }
+    pop();
+  }
+
 }
