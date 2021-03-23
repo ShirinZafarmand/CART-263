198d4c5cd59b40a40bf55678b00503d3c579b90e
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-03-22 20:30:40 -0400

    Project2) detemining the number and width of the bars

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -5,8 +5,24 @@ Project2- Prototype
 shirin zafarmand
 
 **************************************************/
+
+var u;
+var count;
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //number of vertical lines
+  u = 100;
+  //overall width of the lines
+  let widthExtra = windowWidth;
+  //adjusting the lines based of the width
+  count = int(widthExtra/u);
+
+
 }
 
 function draw() {
+ noStroke();
+ background(0);
+
 }
