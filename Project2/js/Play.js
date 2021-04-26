450e346265ed7423b712e92e456cb833f74be29f
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-04-26 04:22:22 -0400

    Project2) the sample gate between the trees

--- Project2/js/Play.js
+++ Project2/js/Play.js
@@ -17,15 +17,18 @@ class Play extends Phaser.Scene{
 
 
    //  Our static TileSprite that will just receive collide events
-   let bamboo = this.add.tileSprite(400, 300, 10 * 3, 1000 * 4, 'player');
-
-   let bamboo1 = this.add.tileSprite(500, 300, 10 * 3, 1000 * 4, 'player');
-
+   let bamboo = this.add.tileSprite(400, 300, 7 * 3, 1000 * 4, 'player');
+   let bamboo1 = this.add.tileSprite(500, 300, 7 * 3, 1000 * 4, 'player');
+   let bamboo2 = this.add.tileSprite(600, 300, 7 * 3, 1000 * 4, 'player');
 
    this.physics.add.existing(bamboo, true);
    this.physics.add.existing(bamboo1, true);
+   this.physics.add.existing(bamboo2, true);
    this.physics.add.collider(block, bamboo);
    this.physics.add.collider(block, bamboo1);
+   this.physics.add.collider(block, bamboo2);
+
+  let rectan = this.add.rectangle(500, 400, 60, 30, 0xff6699);
 }
 
 
