9af22d3da0dac9b3ff0f723cde7fb993a0a0907f
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2021-04-27 18:33:27 -0400

    Project2) background color

--- Project2/js/Play.js
+++ Project2/js/Play.js
@@ -7,32 +7,38 @@ class Play extends Phaser.Scene{
   }
 
   create(){
+    this.cameras.main.backgroundColor.setTo(255,255,255);
     //  Our dynamic TileSprite that will bounce around
-   var block = this.physics.add.sprite( 450, 100, `golden-poo`);
-   this.physics.add.existing(block, false);
+   this.block = this.physics.add.sprite( 450, 100, `golden-poo`);
+   this.physics.add.existing(this.block, false);
 
-   block.body.setVelocity(130, 180);
-   block.body.setBounce(1, 1);
-   block.body.setCollideWorldBounds(true);
+   this.block.body.setVelocity(130, 180);
+   this.block.body.setBounce(1, 1);
+   this.block.body.setCollideWorldBounds(true);
 
 
    //  Our static TileSprite that will just receive collide events
-   let bamboo = this.add.tileSprite(400, 300, 7 * 3, 1000 * 4, 'player');
-   let bamboo1 = this.add.tileSprite(500, 300, 7 * 3, 1000 * 4, 'player');
-   let bamboo2 = this.add.tileSprite(600, 300, 7 * 3, 1000 * 4, 'player');
+   this.bamboo = this.add.tileSprite(400, 300, 7 * 3, 1000 * 4, 'player');
+   this.bamboo1 = this.add.tileSprite(500, 300, 7 * 3, 1000 * 4, 'player');
+   this.bamboo2 = this.add.tileSprite(600, 300, 7 * 3, 1000 * 4, 'player');
 
-   this.physics.add.existing(bamboo, true);
-   this.physics.add.existing(bamboo1, true);
-   this.physics.add.existing(bamboo2, true);
-   this.physics.add.collider(block, bamboo);
-   this.physics.add.collider(block, bamboo1);
-   this.physics.add.collider(block, bamboo2);
+   this.physics.add.existing(this.bamboo, true);
+   this.physics.add.existing(this.bamboo1, true);
+   this.physics.add.existing(this.bamboo2, true);
+   this.physics.add.collider(this.block, this.bamboo);
+   this.physics.add.collider(this.block, this.bamboo1);
+   this.physics.add.collider(this.block, this.bamboo2);
+  this.color = 0xff5878
+  this.rectan = this.add.rectangle(500, 400, 60, 60, this.color);
 
-  let rectan = this.add.rectangle(500, 400, 60, 30, 0xff6699);
+  this.physics.add.collider( this.block, this.rectan, this.exit);
+  this.sound.add('coin');
 }
 
-
-
+exit(){
+  //when the player overlays the golden poo, the image o golden poo disapears
+this.sound.play('coin');
+}
 
   //moving the player with arrow keys
   update(){
